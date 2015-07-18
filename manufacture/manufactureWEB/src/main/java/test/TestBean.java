4246c0929c6efdd3ab81c59d373cb67c5ec980bf
ocalik <vincent.segouin@gmail.com>
2015-07-18 16:49:35 +0200

    clean dependencies

---
+++ manufacture/manufactureWEB/src/main/java/test/TestBean.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestBean {
+	private String toto;
+	public String getToto() {
+		return "hello";
+	}
+
+	public void setToto(String toto) {
+		this.toto = toto;
+	}
+	
+}
