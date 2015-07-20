4fc85d11263d55760d2298f79beb04e378d05d49
ocalik <vincent.segouin@gmail.com>
2015-07-20 19:33:54 +0200

    add spring dependencies

--- manufacture/manufactureENTITY/src/main/java/manufacture/entity/user/ProfessionnalCustomer.java
+++ manufacture/manufactureENTITY/src/main/java/manufacture/entity/user/ProfessionnalCustomer.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
