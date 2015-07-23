9928c31f966fa6382ab0a99bf076567035b39736
Kahloul Imen <imen.kahloul@al24.fr>
2015-07-23 16:29:40 +0200

    ajouter, modifier quantite et supprimer un produit du panier

--- manufacture/manufactureWEB/src/main/java/manufacture/web/cart/ManagedBeanCart.java
+++ manufacture/manufactureWEB/src/main/java/manufacture/web/cart/ManagedBeanCart.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import manufacture.entity.cart.CartProduct;
 import manufacture.entity.product.ConstructorProduct;
 import manufacture.entity.product.Product;
+import manufacture.entity.user.User;
+import manufacture.facade.cart.CartSpecificCustomer;
 import manufacture.ifacade.cart.ICartSpecificCustomer;
 import manufacture.ifacade.catalog.ICatalog;
 //import manufacture.web.catalogBean.ManagedBeanCatalog.Produit;
@@ -25,69 +28,193 @@ import org.springframework.stereotype.Component;
 //@Component
 //@Scope(value="session")
 
-@ManagedBean(name="mbCart")
+@ManagedBean(name = "mbCart")
 @SessionScoped
 public class ManagedBeanCart {
-	
+
 	private Logger log = Logger.getLogger(ManagedBeanCart.class);
 
-	private BeanFactory bf = ClassPathLoader.getFacadeBeanFactory();
-	private ICartSpecificCustomer proxyCart = (ICartSpecificCustomer) bf.getBean(ICartSpecificCustomer.class);
-	private ICatalog proxyCatalog = (ICatalog) bf.getBean(ICatalog.class);
+	@ManagedProperty(value = "#{cartSpecificCustomer}")
+	private ICartSpecificCustomer proxyCart;
+
+	@ManagedProperty(value = "#{catalog}")
+	private ICatalog proxyCatalog;
 	
-	private int idSelectedProduct ;
-	private int quantity ;
-	
+//	@ManagedProperty(value = "#{specificUserCart}")
+	CartSpecificCustomer specificUserCart = new CartSpecificCustomer();
+
+	private int idSelectedProduct;
+	private Product selectedProduct;
+	private int quantity;
+
 	private List<ConstructorProduct> listeProductBrute;
 
 	private int productStock;
-	
+
 	private List<CartProduct> panier = new ArrayList<CartProduct>();
-	
+
 	@PostConstruct
-	void init()
-	{
+	void init() {
 		listeProductBrute = proxyCatalog.getAllConstructorProduct();
 	}
-	
-	public void addProductToCart(){
+
+	public void addProductToCartBeta() {
 		CartProduct cartProduct = new CartProduct();
 		Product product = getProductFromLocalListeById(idSelectedProduct);
-		log.info("Product ID : " + product.getIdProduct());
-		log.info("Product Name : " + product.getProductRef().getProductName());
 		cartProduct.setProduct(product);
 		cartProduct.setQuantity(quantity);
-		log.info("Product Quantity : " + quantity);
-//		proxyCart.addProductToCart(cartProduct);
-		log.info("=====================================  Before cartProduct  ===================================== ");
+		// proxyCart.addProductToCart(cartProduct);
 		panier.add(cartProduct);
-		log.info("=====================================  After cartProduct  ===================================== ");
-		}
-	
-//	public List<CartProduct> getAllCartProducts() {
-//		return panier;	
-//	}
-	
-	public void getStockByProductId(){
-		for (Product product : listeProductBrute) {
-			if (product.getIdProduct()== idSelectedProduct) {
-				productStock = product.getStock();
-				break ;
+	}
+
+	public void addProductToCart() {
+		CartProduct cartProduct = new CartProduct();
+		Product product = getProductFromLocalListeById(idSelectedProduct);
+		int cartProductQuantity = cartProduct.getQuantity();
+		boolean isNewProductInCart = true;
+		if (quantity > 0) {
+			for (CartProduct cp : panier) {
+				if (cp.getProduct().getIdProduct() == idSelectedProduct) {
+					isNewProductInCart = false;
+					cartProductQuantity = cp.getQuantity();
+					cartProductQuantity += quantity ;
+					if (cartProductQuantity < cp.getProduct().getStock()) {
+						cp.setQuantity(cartProductQuantity);
+//						proxyCart.updateQuantityProduct(cp.getIdCartProduct(), cartProductQuantity + quantity);
+					} else {
+						cp.setQuantity(cp.getProduct().getStock());
+					}
+					break;
+				}
+			}
+			if (isNewProductInCart) {
+				cartProduct.setProduct(product);
+				cartProduct.setQuantity(quantity);
+				panier.add(cartProduct);
+				// proxyCart.addProductToCart(cartProduct);
 			}
 		}
 	}
-	
-	public Product getProductFromLocalListeById(int idProduct){
+
+	// public List<CartProduct> getAllCartProducts() {
+	// return panier;
+	// }
+
+	public int getStockByProductId(int idProduct) {
+		int result = 0;
+		for (Product product : listeProductBrute) {
+			if (product.getIdProduct() == idProduct) {
+				result = product.getStock();
+				break;
+			}
+		}
+		return result;
+	}
+
+	public Product getProductFromLocalListeById(int idProduct) {
 		Product result = new Product();
 		for (Product product : listeProductBrute) {
-			if (product.getIdProduct()== idProduct) {
+			if (product.getIdProduct() == idProduct) {
 				result = product;
-				break ;
+				break;
 			}
 		}
-		return result ;
+		return result;
+	}
+
+	public CartProduct getProductFromCartListeById(int idProduct) {
+		CartProduct result = new CartProduct();
+		for (CartProduct cartProduct : panier) {
+			if (cartProduct.getProduct().getIdProduct() == idProduct) {
+				result = cartProduct;
+				break;
+			}
+		}
+		return result;
+	}
+
+	// verifier que l'id retourne est bien celui du produit en question
+	// eventuellement ajouter l'id en parametre
+	public void incrementProductQuantity(int idProduct) {
+		CartProduct cartProduct = getProductFromCartListeById(idProduct);
+		int cartProductQuantity = cartProduct.getQuantity();
+		if (cartProductQuantity < cartProduct.getProduct().getStock()) {
+			cartProductQuantity++;
+		} else {
+			cartProductQuantity = cartProduct.getProduct().getStock();
+		}
+		cartProduct.setQuantity(cartProductQuantity);
+		log.info("Product Quantity : " + cartProduct.getQuantity());
+	}
+
+	public void decrementProductQuantity(int idProduct) {
+		CartProduct cartProduct = getProductFromCartListeById(idProduct);
+		int cartProductQuantity = cartProduct.getQuantity();
+		if (cartProductQuantity > 1) {
+			cartProductQuantity--;
+		} else {
+			cartProductQuantity = 1;
+			// plus besoin de le supprimer de la liste car le user met 
+			//au minimum la qtt 1, sinon il est assez intelligent pour 
+			//le supprimer s'il en veut '0' article
+//			deleteProductFromCart(idProduct);
+//			proxyCart.deleteProductFromCart(cartProduct);
+		}
+		cartProduct.setQuantity(cartProductQuantity);
+		log.info("Product Quantity : " + cartProduct.getQuantity());
 	}
 	
+	public void deleteProductFromCart(int idProduct){
+		CartProduct cartProduct = new CartProduct();
+		Product product = getProductFromLocalListeById(idProduct);
+		for (CartProduct cp : panier) {
+			if (cp.getProduct().getIdProduct() == idProduct) {
+				panier.remove(cp);
+//				proxyCart.deleteProductFromCart(cp);
+				break;
+				}
+			}
+	}
+	
+	//TODO this method
+	//Verifier si c'est ce qu'il faut faire car je ne vois pas la difference entre 
+	public void cleanCart (){
+		panier = new ArrayList<>();
+//		panier.removeAll(panier);
+//		proxyCart.cleanCart(idCart);	//il faut récuperer l'idCart du panier
+	}
+	
+	public void deleteCart (){
+		panier.removeAll(panier);
+//		for (CartProduct cp : panier) {
+//			proxyCart.deleteProductFromCart(cp);
+//		}	
+	}
+	
+	// enregistre le panier pour qu'il soit visible ds les autres managerd bean
+	public void storeCart (){
+		// Les autres parametres pour le panier seront ajoutes apres la validation du paiement
+		for (CartProduct cp : panier) {
+			specificUserCart.addProductToCart(cp);
+		}
+		log.info("============>>>>> JUSQUE LA, CA MARCHE 1 <<<<<============");
+		specificUserCart.createNewCart(1);
+		log.info("============>>>>> JUSQUE LA, CA MARCHE 2 <<<<<============");
+		
+	}
+	
+	
+//	================================================================
+//	==============>>>>>> MOVE TO AN OTHER MBEAN <<<<<<==============
+//	================================================================
+	
+//	public void validateCart (){
+//		proxyCart.createNewCart(userBean.getUser().getIdUser());
+//		for (CartProduct cp : panier) {
+//			proxyCart.addProductToCart(cp);
+//		}
+//	}
+
 	public List<ConstructorProduct> getListeProductBrute() {
 		return listeProductBrute;
 	}
@@ -95,8 +222,6 @@ public class ManagedBeanCart {
 	public void setListeProductBrute(List<ConstructorProduct> listeProductBrute) {
 		this.listeProductBrute = listeProductBrute;
 	}
-	
-	
 
 	public int getIdSelectedProduct() {
 		return idSelectedProduct;
@@ -122,8 +247,6 @@ public class ManagedBeanCart {
 		this.productStock = productStock;
 	}
 
-
-
 	public List<CartProduct> getPanier() {
 		return panier;
 	}
@@ -132,9 +255,19 @@ public class ManagedBeanCart {
 		this.panier = panier;
 	}
 
+	public void setProxyCart(ICartSpecificCustomer proxyCart) {
+		this.proxyCart = proxyCart;
+	}
 
+	public void setProxyCatalog(ICatalog proxyCatalog) {
+		this.proxyCatalog = proxyCatalog;
+	}
+
+	public void setSpecificUserCart(CartSpecificCustomer specificUserCart) {
+		this.specificUserCart = specificUserCart;
+	}
 
 	@Autowired
 	private UserBean userBean;
-	
+
 }
