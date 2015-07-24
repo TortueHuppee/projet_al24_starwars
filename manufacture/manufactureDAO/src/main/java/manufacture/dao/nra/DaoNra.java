958d628e68fe1b758659032d1e336829ebb8d954
Vincent Segouin <vincent.segouin@hotmail.fr>
2015-07-24 23:55:22 +0200

    added comments

--- manufacture/manufactureDAO/src/main/java/manufacture/dao/nra/DaoNra.java
+++ manufacture/manufactureDAO/src/main/java/manufacture/dao/nra/DaoNra.java
@@ -12,7 +12,8 @@ public class DaoNra implements IDaoNra {
 
 	@Override
 	public User getUser(String l, String p) {
-	System.out.println("Tu est dans le DAO mon Grand");
+		System.out.println("Tu est dans le DAO mon Grand");
+		System.out.println("Dommage qu'il ne soit pas fonctionnel, surtout qu'un DAO User existe deja");
 		return new User();
 	}
 
