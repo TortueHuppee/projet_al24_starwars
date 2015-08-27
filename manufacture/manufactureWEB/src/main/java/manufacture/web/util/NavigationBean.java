69fa2aec13923141700f9ae38ebfb5794e7a039c
TortueHuppee <elsa.dillenseger@live.fr>
2015-08-27 13:20:23 +0200

    recuperation du travail de ce matin dans une nouvelle branche

--- manufacture/manufactureWEB/src/main/java/manufacture/web/util/NavigationBean.java
+++ manufacture/manufactureWEB/src/main/java/manufacture/web/util/NavigationBean.java
@@ -2,12 +2,9 @@ package manufacture.web.util;
 
 import java.util.Date;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-
 import manufacture.web.advert.AnnonceManagedBean;
 import manufacture.web.cart.StepsCartManagedBean;
 import manufacture.web.user.LoginBean;
